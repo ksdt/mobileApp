3750d025e55fb5325a259f7b21b76825be938f95
alermi <alermi@ucsd.edu>
2019-03-01 15:56:30 -0800

    Hamburger and KSDT logo added

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Button, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to KSDT!</Text>
+
+      <View>
+        <View style={{flex:1,flexDirection:'row'}}>
+        <TouchableOpacity style={styles.menuButton} onPress={this._onPressButton}>
+              <Image
+                source={require('./assets/menu.png')}
+              />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.ksdtLogoButton} onPress={this._onPressButton}>
+              <Image
+                source={require('./assets/ksdt_logo.png')}
+              />
+        </TouchableOpacity>
+        </View>
+        <ScrollView>
+          <Text>o KSDT!</Text>
+        </ScrollView>
       </View>
+
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  menuButton: {
+      paddingLeft: 16,
+      paddingTop: 35
   },
+  ksdtLogoButton: {
+
+      paddingLeft: 297,
+      paddingTop: 35
+  }
 });
